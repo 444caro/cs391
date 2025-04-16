99609367fc936829872d64a08970f94a3d507260
Caroline Muscara <cjm72@bu.edu>
2025-04-16 19:10:22 -0400

    done

--- mini-projects/mp-5/app/[alias]/page.tsx
+++ mini-projects/mp-5/app/[alias]/page.tsx
@@ -1,5 +1,4 @@
 import getShortenedUrl from "@/app/lib/getShortenedUrl";
-import { get } from "http";
 import { redirect } from "next/navigation";
 
 export default async function Page({ params }: { params: Promise<{ alias: string }>; }) {
